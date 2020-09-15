e1a87e7a9c3c22bd3e635abcbef912cbecce3905
Erickf18 <guillermo.andrade01@epn.edu.ec>
2020-09-15 18:05:15 +0000

    Initial commit

---
+++ heapsort.h
@@ -0,0 +1,13 @@
+#ifndef heapsort_h
+#define heapsort_h
+
+using namespace std;
+
+
+
+void lectura();
+void escritura();
+void heapSort(int b[], int n);
+
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <stdlib.h>
+#include <fstream>
+
+using namespace std;
+
+#include "heapsort.h"
+
+int main() {
+escritura();
+lectura();
+
+system("pause");
+return 0;
+}
+
+
+
+
